40118bb2abbffe46a400bf1dea6e1e2fb53df3f7
mounagowda3 <53967065+mounagowda3@users.noreply.github.com>
2024-10-11 17:09:30 +0530

    UI changes

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,23 +2,16 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on: any, config: any) {
-      // Implement node event listeners here
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'results',
+      overwrite: false,
+      html: true,
+      json: true,
     },
-    specPattern: 'cypress/integration/*.ts',
-    screenshotOnRunFailure: true,
-    screenshotsFolder: 'cypress/screenshots',
-  },
-});
+      specPattern: 'cypress/integration/*.ts',
+      screenshotOnRunFailure: true,
+      screenshotsFolder: 'cypress/screenshots',
+    },
+  });
 
-module.exports = defineConfig({
-    e2e: {
-        reporter: 'mocha-spec', // Specify the reporter here
-        reporterOptions: {
-            // Options for the reporter can be specified here
-            "reporterOptions": {
-                "reporter": "spec",
-            }
-        }
-    },
-});
