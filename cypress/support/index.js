2cbeb31c623bbb399f239564699cb54186b996a7
mounagowda3 <53967065+mounagowda3@users.noreply.github.com>
2024-10-11 17:30:03 +0530

    UI Changes

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,12 +1,12 @@
-import { SpecReporter } from 'jasmine-spec-reporter';
+// import { SpecReporter } from 'jasmine-spec-reporter';
 import 'cypress-xpath';
 
 
-jasmine.getEnv().addReporter(new SpecReporter({
-  spec: {
-    displayStacktrace: true,
-    displayDuration: true,
-  }
-}));
+// jasmine.getEnv().addReporter(new SpecReporter({
+//   spec: {
+//     displayStacktrace: true,
+//     displayDuration: true,
+//   }
+// }));
 
 require('cypress-xpath');
